583c9f116987d21956a977ec429d751cf0416ba0
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-06-25 12:50:14 -0300

    init

---
+++ Unidad1/src/main/java/org/upskilling02/unidad1/Unidad1Application.java
@@ -0,0 +1,13 @@
+package org.upskilling02.unidad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unidad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Unidad1Application.class, args);
+    }
+
+}
---
+++ Unidad1/src/test/java/org/upskilling02/unidad1/Unidad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upskilling02.unidad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unidad1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
